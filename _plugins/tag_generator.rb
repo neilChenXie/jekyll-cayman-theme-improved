c74c888af7442c81f1863a0639e5d9a9f5ae057c
Chen Xie <chenxie2016@163.com>
2026-01-05 19:30:57 +0800

    Fix: Enable custom Jekyll plugin execution in GitHub Actions

--- _plugins/tag_generator.rb
+++ _plugins/tag_generator.rb
@@ -5,30 +5,47 @@ module Jekyll
     def generate(site)
       # 如果禁用了标签生成，则退出
       if site.config['skip_tag_generation']
+        Jekyll.logger.info "Tag Generator:", "Tag generation is disabled"
         return
       end
 
-      tags = site.posts.docs.flat_map { |post| post.data['tags'] || [] }.uniq
-      tags.each do |tag|
-        generate_tag_page(site, tag)
+      begin
+        tags = site.posts.docs.flat_map { |post| post.data['tags'] || [] }.uniq.compact
+        
+        if tags.empty?
+          Jekyll.logger.info "Tag Generator:", "No tags found in posts"
+          return
+        end
+
+        Jekyll.logger.info "Tag Generator:", "Found #{tags.length} unique tags"
+        
+        tags.each do |tag|
+          generate_tag_page(site, tag)
+        end
+      rescue => e
+        Jekyll.logger.error "Tag Generator Error:", e.message
+        Jekyll.logger.error "Tag Generator Error:", e.backtrace.join("\n")
       end
     end
 
     private
 
     def generate_tag_page(site, tag)
-      # 检查标签页面是否已经在 _tags_page 中存在
       tag_dir = File.join(site.source, '_tags_page')
       tag_file = File.join(tag_dir, "#{tag}.md")
 
-      # 如果文件不存在，则创建它
-      unless File.exist?(tag_file)
-        FileUtils.mkdir_p(tag_dir) unless File.exist?(tag_dir)
-        
-        content = "---\nlayout: page_tag\ntitle: #{tag}\n---\n"
-        File.write(tag_file, content)
-        
-        Jekyll.logger.info "Tag Generator:", "Generated tag page for '#{tag}'"
+      begin
+        # 如果文件不存在，则创建它
+        unless File.exist?(tag_file)
+          FileUtils.mkdir_p(tag_dir) unless File.exist?(tag_dir)
+          
+          content = "---\nlayout: page_tag\ntitle: #{tag}\n---\n"
+          File.write(tag_file, content)
+          
+          Jekyll.logger.info "Tag Generator:", "Generated tag page for '#{tag}'"
+        end
+      rescue => e
+        Jekyll.logger.error "Tag Generator Error:", "Failed to generate tag '#{tag}': #{e.message}"
       end
     end
   end
